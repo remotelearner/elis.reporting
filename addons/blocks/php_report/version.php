64e9142d61f45b51ebae179eac809bb59a10366b
brendan <brendan@brendan-desktop.(none)>
2012-06-22 12:31:49 -0400

    ELIS 2.2.0.6 release

--- addons/blocks/php_report/version.php
+++ addons/blocks/php_report/version.php
@@ -16,4 +16,4 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $plugin->version = 2011060300;
-$plugin->release = '2.2.0.5 (Build: 20120618)';
+$plugin->release = '2.2.0.6 (Build: 20120522)';
