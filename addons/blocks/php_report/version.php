a48b14c41d266b83a88ece54533addda68847681
Brent Boghosian <brent.boghosian@remote-learner.net>
2012-11-26 11:38:30 -0500

    ELIS 2.2.0.13 release

--- addons/blocks/php_report/version.php
+++ addons/blocks/php_report/version.php
@@ -16,4 +16,4 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $plugin->version = 2011060300;
-$plugin->release = '2.2.0.12 (Build: 20121118)';
+$plugin->release = '2.2.0.13 (Build: 20121124)';
